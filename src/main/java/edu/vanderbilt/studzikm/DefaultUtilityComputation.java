f84a7dd57aac798aded11239e8a65b325aefc81f
Kate Studzinski <kstudzinski@acm.org>
2022-02-16 14:12:58 -0500

    Allow proportionality constant in util comp

--- src/main/java/edu/vanderbilt/studzikm/DefaultUtilityComputation.java
+++ src/main/java/edu/vanderbilt/studzikm/DefaultUtilityComputation.java
@@ -1,14 +1,31 @@
 package edu.vanderbilt.studzikm;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.common.base.Preconditions;
 
 public class DefaultUtilityComputation implements UtilityComputation {
+
+	private Map<Resource, Integer> proportionalityConsts;
+
+	public DefaultUtilityComputation() {
+		this.proportionalityConsts = new HashMap<>();
+	}
+
+	public DefaultUtilityComputation(Map<Resource, Integer> proportionalityConsts) {
+		Preconditions.checkNotNull(proportionalityConsts, "To use default constants, use the default constructor");
+		this.proportionalityConsts = proportionalityConsts;
+	}
 
 	@Override
 	public double compute(World world, Country country) {
 		return country.getResources()
 				.entrySet()
 				.stream()
-				.mapToDouble(e -> e.getKey().getWeight() * e.getValue())
+				.mapToDouble(e -> proportionalityConsts.getOrDefault(e.getKey(), 1) * 
+						e.getKey().getWeight() * 
+						e.getValue())
 				.sum();
 	}
 
