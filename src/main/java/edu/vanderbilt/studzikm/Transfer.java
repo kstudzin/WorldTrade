96da636b373f17793ead4de0a7d6fd63466c3c20
Kate Studzinski <kstudzinski@acm.org>
2022-02-17 22:20:39 -0500

    Add logging to transfer

--- src/main/java/edu/vanderbilt/studzikm/Transfer.java
+++ src/main/java/edu/vanderbilt/studzikm/Transfer.java
@@ -1,7 +1,10 @@
 package edu.vanderbilt.studzikm;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Transfer implements Action{
+	Logger log = LogManager.getLogger(Transfer.class);
 
 	private Resource resource;
 	private Double percent;
@@ -12,6 +15,13 @@ public class Transfer implements Action{
 	}
 
 	public boolean trade(Country sender, Country receiver) {
+		if (sender.getName() == receiver.getName()) {
+			log.warn("Found countries with same name in transfer:\n"
+					+ "\tSender:   " + sender + "\n"
+					+ "\tReciever: " +receiver);
+			return false;
+		}
+
 		Integer senderAmount = sender.getResource(resource);
 		Integer tradeQuantity = (int) (senderAmount * percent);
 		if (tradeQuantity <= 0) {
