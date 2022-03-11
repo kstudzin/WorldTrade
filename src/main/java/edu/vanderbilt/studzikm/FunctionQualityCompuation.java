6a3314f6673d90aad2336de5b76f0dce8baf03fc
Kate Studzinski <kstudzinski@acm.org>
2022-03-11 14:48:12 -0500

    Add FunctionQualityComputation

---
+++ src/main/java/edu/vanderbilt/studzikm/FunctionQualityCompuation.java
@@ -0,0 +1,38 @@
+package edu.vanderbilt.studzikm;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public class FunctionQualityCompuation implements QualityComputation {
+
+	private BiFunction<Double, Double, Double> function;
+	private Map<String, Double> targetProportion = new HashMap<>();
+
+	public FunctionQualityCompuation(BiFunction<Double, Double, Double> function) {
+		this.function = function;
+		targetProportion.put("R1", 1.0);
+		targetProportion.put("R2", 240.25);
+		targetProportion.put("R3", 5.0/4.0);
+		targetProportion.put("R21", 40.75);
+		targetProportion.put("R22", 20.0);
+		targetProportion.put("R23", .25);
+		targetProportion.put("R21'", 0.001);
+		targetProportion.put("R22'", 0.001);
+		targetProportion.put("R23'", 0.001);
+	}
+
+	@Override
+	public double compute(Country country) {
+		Double people = country.getResource("R1") * 1.0;
+
+		return country.getResources()
+				.entrySet()
+				.stream()
+				.mapToDouble(e -> function.apply(targetProportion.get(e.getKey().getName()), 
+						e.getValue() / people) * 
+						e.getKey().getWeight())
+				.sum();
+	}
+
+}
