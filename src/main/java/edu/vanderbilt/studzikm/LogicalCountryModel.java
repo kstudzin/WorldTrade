a4b413f297efb1f43cb7dfb74a01c4f8876615c5
Kate Studzinski <kstudzinski@acm.org>
2022-04-20 10:14:55 -0400

    Initial logical country model

--- src/main/java/edu/vanderbilt/studzikm/LogicalCountryModel.java
+++ src/main/java/edu/vanderbilt/studzikm/LogicalCountryModel.java
@@ -2,6 +2,13 @@ package edu.vanderbilt.studzikm;
 
 import com.microsoft.z3.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class LogicalCountryModel {
 
     private Context ctx;
@@ -9,116 +16,112 @@ public class LogicalCountryModel {
     private Country country;
     private Integer time = 0;
 
-    public LogicalCountryModel(Context context, Country country) {
+    public LogicalCountryModel(Context context, Country country, String assertions) {
         this.ctx = context;
         this.country = country;
         this.solver = context.mkSolver();
-        initialize();
+
+        initialize(assertions);
     }
 
     /**
      * Add initial axioms to model
      */
-    private void initialize() {
+    private void initialize(String asserts) {
         IntNum exstg_popln = ctx.mkInt(country.getResource("R1"));
         IntNum extsg_elmts = ctx.mkInt(country.getResource("R2"));
         IntNum extsg_timbr = ctx.mkInt(country.getResource("R3"));
         IntNum extsg_alloy = ctx.mkInt(country.getResource("R21"));
         IntNum extsg_elctr = ctx.mkInt(country.getResource("R22"));
         IntNum extsg_house = ctx.mkInt(country.getResource("R23"));
-        
-        Sort[] goalInSorts = {ctx.getStringSort(), ctx.getIntSort()};
-        FuncDecl<BoolSort> goal = ctx.mkFuncDecl("Goal", goalInSorts, ctx.getBoolSort());
-        SeqExpr<CharSort> house = ctx.mkString("house");
-        Expr<BoolSort> goalf = ctx.mkApp(goal, house, ctx.mkInt(0));
-
-        ArithExpr<ArithSort> house_rqrmt = ctx.mkMul(exstg_popln, ctx.mkReal(1, 4));
-        BoolExpr house_goal = ctx.mkLt(extsg_house, house_rqrmt);
-        solver.add(ctx.mkImplies(house_goal, ctx.mkEq(goalf, ctx.mkTrue())));
-//        solver.add(ctx.mkImplies(goalf, ));
-
-        FuncDecl<IntSort> score = ctx.mkFuncDecl("Score", ctx.getIntSort(), ctx.getIntSort());
-        IntExpr score_input = ctx.mkIntConst("score_input");
-        IntExpr score_otput = ctx.mkIntConst("score_otput");
-        Expr<IntSort> sapp = ctx.mkApp(score, score_input);
-        Expr<IntSort> sapp2 = ctx.mkApp(score, ctx.mkInt(4));
-
-        solver.add(ctx.mkImplies(ctx.mkEq(score_input, ctx.mkInt(1)), ctx.mkEq(sapp, ctx.mkInt(5))));
-        solver.add(ctx.mkImplies(ctx.mkEq(score_input, ctx.mkInt(2)), ctx.mkEq(sapp, ctx.mkInt(6))));
-        solver.add(ctx.mkImplies(ctx.mkEq(score_input, ctx.mkInt(3)), ctx.mkEq(sapp, ctx.mkInt(7))));
-        solver.add(ctx.mkEq(sapp2, ctx.mkInt(8)));
-        solver.add(ctx.mkEq(ctx.mkApp(score, ctx.mkInt(5)), ctx.mkInt(8)));
-
-
-        solver.add(ctx.mkEq(score_input, ctx.mkInt(2)));
-        solver.add(ctx.mkEq(score.apply(ctx.mkInt(4)), ctx.mkInt(8)));
-
 
         UninterpretedSort resourceSort = ctx.mkUninterpretedSort("ResourceSort");
+        UninterpretedSort scoreSort = ctx.mkUninterpretedSort("ScoreType");
+        UninterpretedSort timeStep = ctx.mkUninterpretedSort("TimeStep");
+
         Expr<UninterpretedSort> house_resrc = ctx.mkConst("house_resrc", resourceSort);
         Expr<UninterpretedSort> elctr_resrc = ctx.mkConst("elctr_resrc", resourceSort);
         Expr<UninterpretedSort> popln_resrc = ctx.mkConst("popln_resrc", resourceSort);
         Expr<UninterpretedSort> elmts_resrc = ctx.mkConst("elmts_resrc", resourceSort);
         Expr<UninterpretedSort> alloy_resrc = ctx.mkConst("alloy_resrc", resourceSort);
         Expr<UninterpretedSort> timbr_resrc = ctx.mkConst("timbr_resrc", resourceSort);
-
-
-        Sort[] goal2Sorts = {ctx.getIntSort()};
-        FuncDecl<UninterpretedSort> goal2 = ctx.mkFuncDecl("Goal2", goal2Sorts, resourceSort);
-        solver.add(ctx.mkAnd(
-                ctx.mkImplies( ctx.mkLt(extsg_house, house_rqrmt), ctx.mkEq(ctx.mkApp(goal2, ctx.mkInt(0)), house_resrc)),
-                ctx.mkImplies( ctx.mkNot(ctx.mkLt(extsg_house, house_rqrmt)), ctx.mkEq(ctx.mkApp(goal2, ctx.mkInt(0)), elctr_resrc))));
+        Expr<UninterpretedSort> house_waste_resrc = ctx.mkConst("house_waste_resrc", resourceSort);
+        Expr<UninterpretedSort> alloy_waste_resrc = ctx.mkConst("alloy_waste_resrc", resourceSort);
+        Expr<UninterpretedSort> elctr_waste_resrc = ctx.mkConst("elctr_waste_resrc", resourceSort);
 
         Sort[] inputInSort = {resourceSort, resourceSort};
         FuncDecl<BoolSort> input = ctx.mkFuncDecl("Input", inputInSort, ctx.getBoolSort());
-        solver.add(
-                ctx.mkAnd(
-                        ctx.mkApp(input, house_resrc, timbr_resrc),
-                        ctx.mkApp(input, house_resrc, popln_resrc),
-                        ctx.mkApp(input, house_resrc, elmts_resrc),
-                        ctx.mkApp(input, house_resrc, alloy_resrc)));
-//                        ctx.mkNot(ctx.mkApp(input, house_resrc, elctr_resrc))));
 
-        // for all x, y, z if input(x, y) = a and input(y, z) = b => input(x, z) = a * b
+        Sort[] resourceSortArr = {resourceSort};
+        FuncDecl<BoolSort> action = ctx.mkFuncDecl("Action", resourceSortArr, ctx.getBoolSort());
+        FuncDecl<BoolSort> goal = ctx.mkFuncDecl("Goal", resourceSortArr, ctx.getBoolSort());
 
-        Expr<UninterpretedSort> x = ctx.mkConst("x", resourceSort);
-        Expr<UninterpretedSort> y = ctx.mkConst("y", resourceSort);
-        Expr<UninterpretedSort> z = ctx.mkConst("z", resourceSort);
-        BoolExpr body = ctx.mkImplies(ctx.mkAnd(ctx.mkApp(input, x, y), ctx.mkApp(input, y, z)), ctx.mkApp(input, z, z));
-        System.out.println("body: " + body);
-        Sort[] sorts = {resourceSort, resourceSort, resourceSort};
-        Symbol[] names = {ctx.mkSymbol("x"), ctx.mkSymbol("y"), ctx.mkSymbol("z")};
-        Quantifier forall = ctx.mkForall(sorts, names, body, 1, null, null, null, null);
-        System.out.println("Forall: " + forall);
-        solver.add(forall);
+        FuncDecl<IntSort> time = ctx.mkFuncDecl("Time", timeStep, ctx.getIntSort());
+        FuncDecl<UninterpretedSort> invTime = ctx.mkFuncDecl("InvTime", ctx.getIntSort(), timeStep);
 
-        //Test implication
-        solver.add(ctx.mkApp(input, alloy_resrc, elmts_resrc));
-        solver.add(ctx.mkApp(input, alloy_resrc, elctr_resrc));
-        solver.add(ctx.mkNot(ctx.mkApp(input, alloy_resrc, timbr_resrc)));
+        Sort[] scoreInSort = {scoreSort, timeStep};
+        FuncDecl<RealSort> score = ctx.mkFuncDecl("Score", scoreInSort, ctx.getRealSort());
 
-        // unsat? Yes
-        // solver.add(ctx.mkApp(input, house_resrc, elctr_resrc));
+        FuncDecl<UninterpretedSort> oneOfOne = ctx.mkFuncDecl("oneOfOne", new Sort[]{}, scoreSort);
+        FuncDecl<UninterpretedSort> twoOfTwo = ctx.mkFuncDecl("twoOfTwo", new Sort[]{}, scoreSort);
+        FuncDecl<UninterpretedSort> threeOfThree = ctx.mkFuncDecl("threeOfThree", new Sort[]{}, scoreSort);
 
-        System.out.println(solver.check());
-        if (solver.check() == Status.SATISFIABLE) {
-            System.out.println("\nPrinting model: ");
-            System.out.println(solver.getModel());
-            System.out.println();
-            System.out.println(solver.getModel().getFuncInterp(score));
-            System.out.println(solver.getModel().evaluate(sapp, true));
-            System.out.println(score.apply(ctx.mkInt(4)));
-            System.out.println(solver.getModel().evaluate(ctx.mkApp(input, house_resrc, elctr_resrc), true));
-            System.out.println(solver.getModel().evaluate(ctx.mkApp(input, house_resrc, alloy_resrc), true));
-            System.out.println(solver.getModel().evaluate(ctx.mkApp(input, alloy_resrc, elctr_resrc), true));
-            System.out.println(solver.getModel().evaluate(body, true));
-            System.out.println(solver.getModel().evaluate(ctx.mkAnd(ctx.mkApp(input, x, y), ctx.mkApp(input, y, z)), true));
-        }
+        Sort[] sorts = {
+                resourceSort,
+                scoreSort,
+                timeStep
+        };
+        Symbol[] sortNames = Arrays.stream(sorts)
+                .map(Sort::getName)
+                .toArray(Symbol[]::new);
 
-        // If new action is input to goal,
+        Supplier<Stream<FuncDecl<?>>> funcDeclSupplier = () ->
+                Stream.concat(
+                        // Add expressions
+                        Stream.of(house_resrc,
+                                        elctr_resrc,
+                                        popln_resrc,
+                                        elmts_resrc,
+                                        alloy_resrc,
+                                        timbr_resrc,
+                                        house_waste_resrc,
+                                        alloy_waste_resrc,
+                                        elctr_waste_resrc
+                                )
+
+                                // Get function declaration from expression
+                                .map(Expr::getFuncDecl),
+
+                        // Add function declarations
+                        Stream.of(input,
+                                action,
+                                goal,
+                                time,
+                                invTime,
+                                score,
+                                oneOfOne,
+                                twoOfTwo,
+                                threeOfThree)
+                );
+        Symbol[] symbols = funcDeclSupplier
+                .get()
+                .map(FuncDecl::getName)
+                .toArray(Symbol[]::new);
+        FuncDecl<?>[] decls = funcDeclSupplier.get()
+                .toArray(FuncDecl[]::new);
+
+        BoolExpr[] assertObjects = ctx.parseSMTLIB2String(asserts, sortNames, sorts, symbols, decls);
+        Solver s = ctx.mkSolver();
+        Arrays.stream(assertObjects)
+                .forEach(s::add);
+
+        System.out.println(s.check());
     }
 
     public Double score(ActionResult<?> result){
+        Action action = result.getAction();
+        String name = action.getName();
+        Action.Type type = action.getType();
 
         System.out.println(solver.check());
         System.out.println(solver.getModel());
