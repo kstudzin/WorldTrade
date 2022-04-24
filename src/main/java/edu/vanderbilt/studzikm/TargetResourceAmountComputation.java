26512437ea94a7c3dc6749c1c6adaa67bfe3e99c
Kate Studzinski <kstudzinski@acm.org>
2022-04-24 18:28:38 -0400

    initialize solver in constructor

--- src/main/java/edu/vanderbilt/studzikm/TargetResourceAmountComputation.java
+++ src/main/java/edu/vanderbilt/studzikm/TargetResourceAmountComputation.java
@@ -10,38 +10,49 @@ public class TargetResourceAmountComputation {
 
     private Collection<Transform> transforms;
     private Context ctx;
+    private Solver solver;
+    private final IntExpr exstg_house;
+    private final IntExpr exstg_popln;
+    private final IntExpr exstg_elctr;
+    private final IntExpr elctr_rqrmt;
+    private final IntExpr house_rqrmt;
+    private final IntExpr house_timbr;
+    private final IntExpr total_alloy;
+    private final IntExpr total_elmts;
+    private final IntExpr alloy_waste;
+    private final IntExpr elctr_waste;
+    private final IntExpr house_waste;
 
     public TargetResourceAmountComputation(TransformFactory transformFactory, Context ctx) {
         // TODO: Encapsulate transforms as logic
         this.transforms = transformFactory.getTransforms();
         this.ctx = ctx;
-    }
+        this.solver = this.ctx.mkSolver();
 
-    Map<String, Integer> compute(Country country) {
-        IntNum exstg_house = ctx.mkInt(country.getResource("R23"));
-        IntNum exstg_popln = ctx.mkInt(country.getResource("R1"));
-        IntNum exstg_elctr = ctx.mkInt(country.getResource("R22"));
+        this.exstg_house = ctx.mkIntConst("exstg_house");
+        this.exstg_popln = ctx.mkIntConst("exstg_popln");
+        this.exstg_elctr = ctx.mkIntConst("exstg_elctr");
 
         IntExpr house_alloy = ctx.mkIntConst("house_alloy"); // Amount of alloys needed for the houses
-        IntExpr house_timbr = ctx.mkIntConst("house_timbr"); // Amount of timber needed for the houses
+        this.house_timbr = ctx.mkIntConst("house_timbr"); // Amount of timber needed for the houses
         IntExpr house_elmts = ctx.mkIntConst("house_elmts"); // Amount of elements needed for the houses
-        IntExpr house_waste = ctx.mkIntConst("house_waste");
+        this.house_waste = ctx.mkIntConst("house_waste");
 
         IntExpr elctr_elmts = ctx.mkIntConst("elctr_elmts");
         IntExpr elctr_alloy = ctx.mkIntConst("elctr_alloy");
-        IntExpr elctr_waste = ctx.mkIntConst("elctr_waste");
+        this.elctr_waste = ctx.mkIntConst("elctr_waste");
 
-        IntExpr total_elmts = ctx.mkIntConst("total_elmts");
-        IntExpr total_alloy = ctx.mkIntConst("total_alloy");
+        this.total_elmts = ctx.mkIntConst("total_elmts");
+        this.total_alloy = ctx.mkIntConst("total_alloy");
 
         IntExpr alloy_elmts = ctx.mkIntConst("alloy_elmts");
-        IntExpr alloy_waste = ctx.mkIntConst("alloy_waste");
+        this.alloy_waste = ctx.mkIntConst("alloy_waste");
 
         RealExpr house_popln_ratio = ctx.mkRealConst("house_popln_ratio");
-        IntExpr house_rqrmt = ctx.mkIntConst("house_rqrmt"); // Number of houses to be built
+        this.house_rqrmt = ctx.mkIntConst("house_rqrmt"); // Number of houses to be built
 
         IntExpr elctr_popln_ratio = ctx.mkIntConst("elctr_popln_ratio");
-        IntExpr elctr_rqrmt = ctx.mkIntConst("elctr_rqrmt");
+        this.elctr_rqrmt = ctx.mkIntConst("elctr_rqrmt");
 
         IntNum zero = ctx.mkInt(0);
         IntNum house_alloy_mult = ctx.mkInt(3);
@@ -55,7 +66,6 @@ public class TargetResourceAmountComputation {
         IntNum alloy_elmts_mult = ctx.mkInt(2);
         IntNum alloy_popln_rqmt = ctx.mkInt(1);
 
-        Solver solver = ctx.mkSolver();
         solver.add(ctx.mkEq(house_popln_ratio, ctx.mkReal(1, 4)));
         solver.add(ctx.mkEq(elctr_popln_ratio, ctx.mkInt(25)));
 
@@ -64,9 +74,9 @@ public class TargetResourceAmountComputation {
         solver.add(ctx.mkEq(house_elmts, house_rqrmt));
         solver.add(ctx.mkEq(house_waste, house_rqrmt));
         solver.add(ctx.mkGe(ctx.mkAdd(house_rqrmt, exstg_house),
-                            ctx.mkMul(house_popln_ratio, exstg_popln)));
+                ctx.mkMul(house_popln_ratio, exstg_popln)));
         solver.add(ctx.mkImplies(ctx.mkGt(house_rqrmt, zero),
-                                 ctx.mkGe(exstg_popln, house_popln_rqmt)));
+                ctx.mkGe(exstg_popln, house_popln_rqmt)));
         solver.add(ctx.mkGe(house_rqrmt,  zero));
 
         solver.add(ctx.mkAnd(
@@ -87,7 +97,14 @@ public class TargetResourceAmountComputation {
         solver.add(ctx.mkEq(alloy_elmts, ctx.mkMul(alloy_elmts_mult, total_alloy)));
         solver.add(ctx.mkEq(alloy_waste, total_alloy));
         solver.add(ctx.mkImplies(ctx.mkGt(total_alloy, zero),
-                                 ctx.mkGe(exstg_popln, alloy_popln_rqmt)));
+                ctx.mkGe(exstg_popln, alloy_popln_rqmt)));
+    }
+
+    Map<String, Integer> compute(Country country) {
+        solver.push();
+        solver.add(ctx.mkEq(exstg_house, ctx.mkInt(country.getResource("R23"))));
+        solver.add(ctx.mkEq(exstg_popln, ctx.mkInt(country.getResource("R1"))));
+        solver.add(ctx.mkEq(exstg_elctr, ctx.mkInt(country.getResource("R22"))));
 
         Status status = solver.check();
         if (status != status.SATISFIABLE) {
@@ -116,6 +133,8 @@ public class TargetResourceAmountComputation {
         proportions.put("R21'", Integer.valueOf(alloy_waste_expct.toString()));
         proportions.put("R22'", Integer.valueOf(elctr_waste_expct.toString()));
         proportions.put("R23'", Integer.valueOf(house_waste_expct.toString()));
+
+        solver.pop();
         return proportions;
     }
 
