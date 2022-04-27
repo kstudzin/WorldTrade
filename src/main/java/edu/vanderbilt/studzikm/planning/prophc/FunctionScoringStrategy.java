8e0b4fb2d32889052e8397bd88ae88f05f7505ef
Kate Studzinski <kstudzinski@acm.org>
2022-04-27 10:06:13 -0400

    Minimum score greater than zero

--- src/main/java/edu/vanderbilt/studzikm/planning/prophc/FunctionScoringStrategy.java
+++ src/main/java/edu/vanderbilt/studzikm/planning/prophc/FunctionScoringStrategy.java
@@ -1,13 +1,17 @@
 package edu.vanderbilt.studzikm.planning.prophc;
 
 import org.apache.commons.collections4.queue.CircularFifoQueue;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.function.Function;
 
 public class FunctionScoringStrategy implements ScoringStrategy {
 
-    private static final double c = 0.60793;
-    private Function<Integer, Double> func = i -> c / Math.pow(i, 2);
+    private Logger log = LogManager.getLogger(FunctionScoringStrategy.class);
+
+    private static final double c = 0.83188;
+    private Function<Integer, Double> func = i -> c / Math.pow(i, 3);
 
     @Override
     public double compute(CircularFifoQueue<Boolean> history) {
@@ -17,6 +21,11 @@ public class FunctionScoringStrategy implements ScoringStrategy {
                 sum += func.apply(history.size() - i);
             }
         }
+
+        if (sum == 0.0) {
+            sum = 0.2;
+        }
+        log.trace(String.format("History: %s (Score: %f)", history, sum));
         return sum;
     }
 
