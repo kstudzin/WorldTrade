85328ba803677df8f8288abd3d79a201b5518628
Kate Studzinski <kstudzinski@acm.org>
2022-04-27 10:51:45 -0400

    Change to exponential scoring strategy

--- src/main/java/edu/vanderbilt/studzikm/planning/prophc/FunctionScoringStrategy.java
+++ src/main/java/edu/vanderbilt/studzikm/planning/prophc/FunctionScoringStrategy.java
@@ -10,23 +10,23 @@ public class FunctionScoringStrategy implements ScoringStrategy {
 
     private Logger log = LogManager.getLogger(FunctionScoringStrategy.class);
 
-    private static final double c = 0.83188;
-    private Function<Integer, Double> func = i -> c / Math.pow(i, 3);
+    private Function<Integer, Double> func = i -> Math.pow(i, 2);
+    private Function<Integer, Integer> sumOfSquares = i ->
+            (i * (i + 1) * ((2 * i) + 1)) / 6;
 
     @Override
     public double compute(CircularFifoQueue<Boolean> history) {
         Double sum = 0.0;
+        Integer max = sumOfSquares.apply(history.size());
         for (int i = 0; i < history.size(); i++) {
             if (history.get(i)) {
-                sum += func.apply(history.size() - i);
+                sum += func.apply(i);
             }
         }
 
-        if (sum == 0.0) {
-            sum = 0.2;
-        }
-        log.trace(String.format("History: %s (Score: %f)", history, sum));
-        return sum;
+        Double score = Math.max(0.05, sum / max);
+        log.trace(String.format("History: %s (Score: %f)", history, score));
+        return score;
     }
 
 }
